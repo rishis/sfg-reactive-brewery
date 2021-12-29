2c945211b7940a2fa7338f187db8926bef042bdf
Rishi Shehrawat <rishishehrawat@gmail.com>
2021-12-29 12:26:04 +0530

    refactor mutating operations

--- src/main/java/guru/springframework/sfgrestbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/sfgrestbrewery/web/controller/BeerController.java
@@ -80,8 +80,9 @@ public class BeerController {
     }
 
     @PutMapping("beer/{beerId}")
-    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody @Validated BeerDto beerDto){
-        return new ResponseEntity<>(beerService.updateBeer(beerId, beerDto), HttpStatus.NO_CONTENT);
+    public ResponseEntity    updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody @Validated BeerDto beerDto){
+        beerService.updateBeer(beerId, beerDto);
+        return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("beer/{beerId}")
@@ -89,7 +90,7 @@ public class BeerController {
 
         beerService.deleteBeerById(beerId);
 
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return ResponseEntity.ok().build();
     }
 
 }
